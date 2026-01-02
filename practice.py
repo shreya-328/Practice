b217ca98a9657e8d1cb4089d2287790f57e77463
Shreya-328 <shreyaanigam03@gmail.com>
2026-01-02 23:50:43 +0530

    Time complexity questions

--- practice.py
+++ practice.py
@@ -701,38 +701,19 @@
 #     s = "forgeeksskeegfor"
 #     print(getLongestPal(s))
 
-class Node:
-    def __init__(self, x):
-        self.data = x
-        self.next = None
 
-def detectLoop(head):
-    st = set()
+# time complexity questions
+# a = 0
+# b = 0
+# for i in range(N):
+#   a = a + random()
 
-    while head is not None:
+# for i in range(M):
+#   b= b + random()
 
-        # if this node is already present
-        # in hashmap it means there is a cycle
-        if head in st:
-            return True
-
-        # if we are seeing the node for
-        # the first time, insert it in hash
-        st.add(head)
-
-        head = head.next
-
-    return False
-
-if __name__ == "__main__":
-
-    head = Node(1)
-    head.next = Node(3)
-    head.next.next = Node(4)
-
-    head.next.next.next = head.next
-
-    if detectLoop(head):
-        print("true")
-    else:
-        print("false")
+def fun(n,m):
+    for i in range(n):
+        print(i)
+    for i in range(m):
+        print(i)
+print(fun(1,2))
