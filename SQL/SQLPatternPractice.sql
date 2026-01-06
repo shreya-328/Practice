08a18349d10e47c70c1de89ffe0e24fe2b46c4bb
Shreya-328 <shreyaanigam03@gmail.com>
2026-01-06 20:02:00 +0530

    Add new data

--- SQL/SQLPatternPractice.sql
+++ SQL/SQLPatternPractice.sql
@@ -87,4 +87,84 @@
 -- )
 -- select count(*) from cte where totalo=1
 
+-- ALTER TABLE dbo.Orders
+-- ADD 
+--     order_amount INT,
+--     seller_id INT;
+-- GO
+-- CREATE TABLE dbo.Users (
+--     user_id INT PRIMARY KEY,
+--     signup_date DATE
+-- );
+
+-- SELECT * 
+-- FROM INFORMATION_SCHEMA.TABLES
+-- WHERE TABLE_NAME = 'Orders';
+
+-- CREATE TABLE dbo.Sellers (
+--     seller_id INT PRIMARY KEY,
+--     seller_name VARCHAR(50)
+-- );
+
+-- CREATE TABLE dbo.Order_Items (
+--     order_item_id INT PRIMARY KEY,
+--     order_id INT,
+--     product_id INT,
+--     quantity INT
+-- );
+
+-- UPDATE dbo.Orders
+-- SET order_amount = CASE order_id
+--     WHEN 101 THEN 500
+--     WHEN 102 THEN 700
+--     WHEN 103 THEN 1200
+--     WHEN 104 THEN 800
+--     WHEN 105 THEN 400
+--     WHEN 106 THEN 20000
+--     WHEN 107 THEN 18000
+--     WHEN 108 THEN 22000
+--     WHEN 109 THEN 15000
+--     WHEN 110 THEN 17000
+--     WHEN 111 THEN 9000
+--     WHEN 112 THEN 3000
+-- END,
+-- seller_id = CASE 
+--     WHEN order_id IN (101,102,103,104) THEN 1
+--     WHEN order_id IN (105,106,107) THEN 2
+--     ELSE 3
+-- END;
+
+-- INSERT INTO dbo.Users VALUES
+-- (1, '2023-12-25'),
+-- (2, '2024-01-01'),
+-- (3, '2024-01-05'),
+-- (4, '2024-01-10'),
+-- (5, '2024-01-28');
+
+-- INSERT INTO dbo.Sellers VALUES
+-- (1, 'Seller A'),
+-- (2, 'Seller B'),
+-- (3, 'Seller C');
+
+-- INSERT INTO dbo.Order_Items VALUES
+-- (1, 106, 1, 1),
+-- (2, 106, 2, 1),
+-- (3, 106, 3, 1),
+-- (4, 106, 4, 1),
+-- (5, 106, 5, 1),
+-- (6, 106, 6, 1),   -- order 106 has 6 items
+
+-- (7, 107, 1, 1),
+-- (8, 107, 2, 1),
+
+-- (9, 108, 1, 1),
+-- (10,108, 2, 1),
+-- (11,108, 3, 1),
+-- (12,108, 4, 1),
+-- (13,108, 5, 1),
+-- (14,108, 6, 1),
+-- (15,108, 7, 1);   -- order 108 has 7 items
+
+-- 
+
 -- Q.5. 
