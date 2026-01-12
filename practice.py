3079e6939fbad222b9583fd71ce3459a42845747
Shreya-328 <shreyaanigam03@gmail.com>
2026-01-12 21:51:13 +0530

    Added another approach

--- practice.py
+++ practice.py
@@ -841,12 +841,23 @@
 # n = int(input())
 # print(oppositeFaceOfDice(n))
 
+# def nthTermOfAP(a1, a2, n):
+#     nthTerm = a1
+#     d = a2 - a1
+#     for i in range(1, n):
+#         nthTerm += d
+#     return nthTerm
+
+
+# a1 = 2
+# a2 = 3
+# n = 4
+# print(nthTermOfAP(a1, a2, n))
+
 def nthTermOfAP(a1, a2, n):
-    nthTerm = a1
-    d = a2 - a1
-    for i in range(1, n):
-        nthTerm += d
-    return nthTerm
+    # using formula to find the
+    # Nth term t(n) = a(1) + (n-1)*d
+    return a1 + (n - 1) * (a2 - a1)
 
 
 a1 = 2
