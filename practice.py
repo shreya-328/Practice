9f9ac3c2739f507fe37ecc414cc49d0e2088df53
Shreya-328 <shreyaanigam03@gmail.com>
2026-01-16 23:39:00 +0530

    add overlapping rectangles

--- practice.py
+++ practice.py
@@ -937,4 +937,33 @@ if checkValidity(a, b, c):
     print("Valid") 
 else:
     print("Invalid")
-    
+
+
+# overlapping rectangle
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def do_overlap(l1, r1, l2, r2):
+    # If one rectangle is to the left of the other
+    if l1.x > r2.x or l2.x > r1.x:
+        return False
+
+    # If one rectangle is above the other
+    if r1.y > l2.y or r2.y > l1.y:
+        return False
+
+    return True
+
+# Driver code
+if __name__ == "__main__":
+    l1 = Point(0, 10)
+    r1 = Point(10, 0)
+    l2 = Point(5, 5)
+    r2 = Point(15, 0)
+
+    if do_overlap(l1, r1, l2, r2):
+        print("Rectangles Overlap")
+    else:
+        print("Rectangles Don't Overlap")
