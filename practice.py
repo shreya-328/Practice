de1a13dc7abc85cc38dbcb1d9406f075b641e73c
Shreya-328 <shreyaanigam03@gmail.com>
2026-02-03 22:18:10 +0530

    find the maximum sum of any contiguous subarray of size k

--- practice.py
+++ practice.py
@@ -1153,3 +1153,16 @@
 s = input()
 words = [word for word in s.split(" ")]
 print(" ".join(sorted(list(set(words)))))
+
+# find the maximum sum of any contiguous subarray of size k
+nums = [2, 1, 5, 1, 3, 2]
+k = 3
+
+window_sum = sum(nums[:k])
+max_sum = window_sum
+
+for i in range(k, len(nums)):
+    window_sum = window_sum + nums[i] - nums[i - k]
+    max_sum = max(max_sum, window_sum)
+
+print(max_sum)
