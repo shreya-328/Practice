54aa57cc0006885b2dc4873cfc0c7ea73842144f
Shreya-328 <shreyaanigam03@gmail.com>
2026-02-09 23:32:51 +0530

    split

--- practice.py
+++ practice.py
@@ -1236,25 +1236,35 @@
 #         pass
 # print(netAmount)
 
-import re
-value = []
-items=[x for x in input().split(',')]
-for p in items:
-    if len(p)<6 or len(p)>12:
-        continue
-    else:
-        pass
-    if not re.search("[a-z]",p):
-        continue
-    elif not re.search("[0-9]",p):
-        continue
-    elif not re.search("[A-Z]",p):
-        continue
-    elif not re.search("[$#@]",p):
-        continue
-    elif re.search("\s",p):
-        continue
-    else:
-        pass
-    value.append(p)
-print(",".join(value))
+# import re
+# value = []
+# items=[x for x in input().split(',')]
+# for p in items:
+#     if len(p)<6 or len(p)>12:
+#         continue
+#     else:
+#         pass
+#     if not re.search("[a-z]",p):
+#         continue
+#     elif not re.search("[0-9]",p):
+#         continue
+#     elif not re.search("[A-Z]",p):
+#         continue
+#     elif not re.search("[$#@]",p):
+#         continue
+#     elif re.search("\s",p):
+#         continue
+#     else:
+#         pass
+#     value.append(p)
+# print(",".join(value))
+
+
+l = []
+while True:
+    s = input()
+    if not s:
+        break
+    l.append(tuple(s.split(",")))
+
+print(sorted(l, key=itemgetter(0,1,2)))
