f59d906e9f15211521ae5b9c663e515c36cd0d50
Shreya-328 <shreyaanigam03@gmail.com>
2026-02-11 23:45:41 +0530

    added new

--- practice.py
+++ practice.py
@@ -1268,3 +1268,12 @@ while True:
     l.append(tuple(s.split(",")))
 
 print(sorted(l, key=itemgetter(0,1,2)))
+
+def printDict():
+	d=dict()
+	for i in range(1,21):
+		d[i]=i**2
+	for (k,v) in d.items():	
+		print(v)
+
+printDict()
