2b2ea577ab541867ea5a89b6f5046a4adba6f4c5
Shreya-328 <shreyaanigam03@gmail.com>
2026-02-12 23:31:58 +0530

    using self

--- practice.py
+++ practice.py
@@ -1277,3 +1277,13 @@ def printDict():
 		print(v)
 
 printDict()
+
+class Circle(object):
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return self.radius**2*3.14
+
+aCircle = Circle(2)
+print (aCircle.area())
