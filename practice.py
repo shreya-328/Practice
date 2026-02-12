09fa614d86e2dc9587ea580b07c06979fbab6a70
Shreya-328 <shreyaanigam03@gmail.com>
2026-02-12 23:32:37 +0530

    rectangle

--- practice.py
+++ practice.py
@@ -1287,3 +1287,14 @@ class Circle(object):
 
 aCircle = Circle(2)
 print (aCircle.area())
+
+class Rectangle(object):
+    def __init__(self, l, w):
+        self.length = l
+        self.width  = w
+
+    def area(self):
+        return self.length*self.width
+
+aRectangle = Rectangle(2,10)
+print(aRectangle.area())
