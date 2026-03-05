07092a13f1d568bcbbb39d75287fac8014ab0524
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-05 23:26:32 +0530

    max consectutive ones iii

--- practice.py
+++ practice.py
@@ -1547,3 +1547,21 @@ class Solution:
             ans = max(ans, right - left)
         return ans
         
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        l = 0
+        m = 0
+        
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                k -= 1
+            
+            while k < 0:
+                if nums[l] == 0:
+                    k += 1
+                l += 1
+            
+            m = max(m, r - l + 1)
+        return m
+        
