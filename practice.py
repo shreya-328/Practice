809d303d669a2fba2be101e4cece42f5fd980cb5
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-11 22:13:48 +0530

    number of recent call

--- practice.py
+++ practice.py
@@ -1648,3 +1648,22 @@ class Solution:
                 curr += ch
 
         return curr
+    
+    from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+
+        while self.q[0] < t-3000:
+            self.q.popleft()
+        
+        return len(self.q)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
