aca66f0380b7188a61c2484442f59d9a75b2770f
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-13 23:36:39 +0530

    no of recent call

--- practice.py
+++ practice.py
@@ -1683,3 +1683,22 @@ class Solution:
                 active.append(spent.popleft())
         
         return "Radiant" if active[0]=='R' else "Dire"
+    
+    from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+
+        while self.q[0] < t-3000:
+            self.q.popleft()
+        
+        return len(self.q)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
