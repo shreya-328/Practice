9c122a1ba1075bfe762ef38e5fbd958b45ebeb93
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-16 21:32:00 +0530

    is subsequences

--- practice.py
+++ practice.py
@@ -1718,6 +1718,18 @@ class Solution:
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         return [list(set(nums1) - set(nums2)),list(set(nums2) - set(nums1))]
-                        
+
+        class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        len1 = len(s)
+        len2 = len(t)
+        i = j = 0
+        while i < len1 and j < len2:
+            if t[j] == s[i]:
+                i += 1
+            j += 1
+        if i == len1:
+            return True
+        return False                
 
 
