575a1167b96e8483ad0bf5b6ea58ce7359a76416
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-18 22:32:56 +0530

    no of recent calls

--- practice.py
+++ practice.py
@@ -1747,6 +1747,20 @@ class Solution:
             nums[j] = 0
         
         return nums
-                        
+
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+
+        while self.q[0] < t-3000:
+            self.q.popleft()
+        
+        return len(self.q)
+                
 
 
