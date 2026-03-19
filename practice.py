1736b399037ea587e8b4f5b008c51462dd7bc02e
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-19 22:11:00 +0530

    remove stars

--- practice.py
+++ practice.py
@@ -1762,5 +1762,15 @@ class RecentCounter:
         
         return len(self.q)
                 
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if stack and c == "*":
+                stack.pop()
+            else:
+                stack.append(c)
 
+        return "".join(stack)
+        
 
