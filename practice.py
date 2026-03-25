7a4be6f32a04e04ca9ebaa7d811b3936d3417374
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-25 23:38:27 +0530

    equal row and column

--- practice.py
+++ practice.py
@@ -1830,3 +1830,25 @@ class Solution:
             _walk(root, targetSum, sums, c)
         
         return c[0]
+
+        class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        rows = []
+        for i in range(len(grid)):
+            rows.append(grid[i])
+
+        cols = []
+        for j in range(len(grid)):
+            col = []
+            for i in range(len(grid)):
+                col.append(grid[i][j])
+            cols.append(col)
+
+        count = 0
+        for row in rows:
+            for col in cols:
+                if row == col:
+                    count += 1
+        
+        return count
+
