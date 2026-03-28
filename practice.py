6d78adb8141907f6ab94a0dde1861509c90a90fb
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-28 23:09:45 +0530

    water container

--- practice.py
+++ practice.py
@@ -1872,3 +1872,19 @@ class Solution:
         return str1[:gcd]
 
     
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        l, r = 0, len(height)-1
+
+        while l<r:
+            area = (r-l)* min(height[r],height[l])
+            res = max(res, area)
+
+            if height[l] < height[r]:
+                l+=1
+            elif height[r] < height[l]:
+                r-=1
+            else:
+                l+=1
+        return res
