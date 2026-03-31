d14a1fa195f38934a7ec25b9d4ce58b575e0bc59
Shreya-328 <shreyaanigam03@gmail.com>
2026-03-31 23:44:46 +0530

    jump game

--- practice.py
+++ practice.py
@@ -1914,3 +1914,15 @@ for num in arr:
     if freq[num] == 1:
         print(num)
         break
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        gas = 0
+        for n in nums:
+            if gas < 0:
+                return False
+            elif n > gas:
+                gas = n
+            gas -= 1
+            
+        return True
