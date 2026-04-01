96ded4b0cf1dbafa80e220c33830d7ca3eabd5e4
Shreya-328 <shreyaanigam03@gmail.com>
2026-04-01 23:25:26 +0530

    roman to int

--- practice.py
+++ practice.py
@@ -1926,3 +1926,17 @@ class Solution:
             gas -= 1
             
         return True
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        result = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                result -= roman_to_int[s[i]]
+            else:
+                result += roman_to_int[s[i]]
+        return result
