f1e057e1b2f9c9e04470ce2af5d961c33b5fbffd
Shreya-328 <shreyaanigam03@gmail.com>
2026-04-11 23:25:42 +0530

    group anagrams

--- practice.py
+++ practice.py
@@ -1988,3 +1988,13 @@ class Solution:
             else:
                 l+=1
         return res
+    
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
