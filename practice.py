14bbfeb6b5d9eb3a67de11005fb514ee90c42ceb
Shreya-328 <shreyaanigam03@gmail.com>
2026-04-16 23:58:13 +0530

    trapping rain water

--- practice.py
+++ practice.py
@@ -2087,3 +2087,23 @@ class Solution:
                 candies[i] = max(candies[i], candies[i+1] + 1)
         
         return sum(candies)
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        left_max = height[left]
+        right_max = height[right]
+        water = 0
+
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+        
+        return water
